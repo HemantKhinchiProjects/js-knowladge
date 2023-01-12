1815924f042919a74480019da659c0f3a222085e
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-01-12 18:47:53 +0530

    2 files modified, 1 added

---
+++ functions/function.js
@@ -0,0 +1,8 @@
+//-----------------------Important-------------------------------------//
+//Function Statment or //Function Declartion
+function sample() {
+  console.log('simple Function called');
+  document.innerHTML = 'simple Function called';
+}
+//Export Code
+export { sample };
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Import stylesheets
 import './style.css';
-
+// Import script files
+import { sample } from './functions/function.js';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const functionDisplay = document.querySelector('.simpleFunction');
+functionDisplay.innerHTML = `<h1>JS Starter</h1> ${sample()}`;
