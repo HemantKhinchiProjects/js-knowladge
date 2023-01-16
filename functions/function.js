af4d1bc41a125fd454aba9ca009f939068278e56
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-01-16 14:42:57 +0530

    1 file modified

--- functions/function.js
+++ functions/function.js
@@ -2,7 +2,10 @@
 //Function Statment or //Function Declartion
 function sample() {
   console.log('simple Function called');
-  document.innerHTML = 'simple Function called';
+  const functionDisplay = document.querySelector('.simpleFunction');
+  functionDisplay.innerHTML = `<h1>JS Starter</h1> `;
+
+  return functionDisplay;
 }
 //Export Code
 export { sample };
